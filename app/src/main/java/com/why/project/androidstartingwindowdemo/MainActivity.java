d1230920f8a65c2bf93ad7e2a42bbca7da729c6f
haiyuKing <wanghaiyu99999@126.com>
2018-07-17 15:31:29 +0800

    1、添加应用启动后恢复原有样式的代码

--- app/src/main/java/com/why/project/androidstartingwindowdemo/MainActivity.java
+++ app/src/main/java/com/why/project/androidstartingwindowdemo/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setTheme(R.style.AppTheme);//恢复原有的样式
 		setContentView(R.layout.activity_main);
 	}
 }
